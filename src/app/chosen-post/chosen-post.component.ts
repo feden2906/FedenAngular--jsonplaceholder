3688b3564816a6946584d5d6e066b69d2debf203
Fedenko Maksym <feden2906@gmail.com>
2021-01-22 20:40:05 +0200

    render chosen-posts

---
+++ src/app/chosen-post/chosen-post.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from '../../models/Post';
+
+@Component({
+  selector: 'app-chosen-post',
+  templateUrl: './chosen-post.component.html',
+  styleUrls: ['./chosen-post.component.css']
+})
+export class ChosenPostComponent implements OnInit {
+
+  @Input()
+  post: Post;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
