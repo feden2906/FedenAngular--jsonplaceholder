e7525cf18de8b7bf770c4a48c817fb5a15534ab5
Fedenko Maksym <feden2906@gmail.com>
2021-01-23 17:41:03 +0200

    add home page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  navItems: string[] = ['Posts', 'Comments', 'Photos', 'Users'];
+  navItems = ['', 'Posts', 'Comments', 'Photos', 'Users'];
   constructor() { }
 
   ngOnInit(): void {
